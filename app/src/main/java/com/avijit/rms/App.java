9cc72e89e15ee608d8f9424ec58e164aa5a7d5c2
Avijit Acharjee <avijitach@gmail.com>
2020-04-25 11:57:23 +0600

    Krishna

---
+++ app/src/main/java/com/avijit/rms/App.java
@@ -0,0 +1,14 @@
+package com.avijit.rms;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        }
+    }
+}
